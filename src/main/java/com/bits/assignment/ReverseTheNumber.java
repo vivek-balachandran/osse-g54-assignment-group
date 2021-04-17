9e2da6097b8008efb59960d52e514ca3d9a6262e
Vivek <vivek.lotlikar@gmail.com>
2021-04-17 22:28:27 +0530

    final variable in Reverese the number

--- src/main/java/com/bits/assignment/ReverseTheNumber.java
+++ src/main/java/com/bits/assignment/ReverseTheNumber.java
@@ -5,12 +5,14 @@ public class ReverseTheNumber {
 	public static void main(String[] args) {
 
 		int num = 456789, reversed = 0;
+		int final;
 
 		while (num != 0) {
 			int digit = num % 10;
 			reversed = reversed * 10 + digit;
 			num /= 10;
 		}
+		final = reversed;
 
 		System.out.println("Reversed Number: " + reversed);
 	}
